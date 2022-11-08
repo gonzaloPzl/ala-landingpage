133cd3733dae241c6f66cad57163ceba83228ced
Gonzalo Pozzoli <gonzalopozzoli99@gmail.com>
2022-11-07 23:21:44 -0300

    install react router + install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>LADING PAGE ALA EN CONSTRUCCIÓN</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Ala</h1>
+      
     </div>
   );
 }
